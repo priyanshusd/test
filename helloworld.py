f279e9ca1d990909837a673528368a45fbe1d86a
priyanshusd <51895602+priyanshusd@users.noreply.github.com>
2022-11-24 16:43:17 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,18 @@
 import psycopg2
-print("hello there")
-conn = psycopg2.connect(
-    host="localhost",
-    database="suppliers",
-    user="postgres",
-    password="Abcd1234")
+connection = psycopg2.connect(
+        host="devtoppers.cagadciwq7si.us-east-1.rds.amazonaws.com",
+        database="pythonapp",
+        user="python",
+        password="python123")
+
+cursor = connection.cursor()
+# Executing a SQL query to insert data into  table
+insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (1, 'Iphone12', 1100)"""
+cursor.execute(insert_query)
+connection.commit()
+print("1 Record inserted successfully")
+# Fetch result
+cursor.execute("SELECT * from mobile")
+record = cursor.fetchall()
+print("Result ", record)
 
